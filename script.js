3358854882ac0374f9d4866dc8eb43106bb84da8
Jared Brown <jaredbrown1@hotmail.com>
2021-05-22 16:13:06 -0700

    unsplash api hooked up to application

---
+++ script.js
@@ -0,0 +1,18 @@
+// Unsplash API
+const count = 10;
+const apiKey = "CNwBLKicjNz0kjWY4Tj6ZkzeJXbXXxWB39lKvOHL6jA";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+	try {
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		// Catch Error Here
+	}
+}
+
+// On Load
+getPhotos();
